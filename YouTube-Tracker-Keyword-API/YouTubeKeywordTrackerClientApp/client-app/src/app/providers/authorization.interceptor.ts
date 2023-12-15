43578400d1f7a3c42ad8c6aaa77eb44792fac85c
sekvi <filmatkozlik@gmail.com>
2023-12-15 14:14:47 +0100

    Fixed provided for automatically logging out users with expired token

--- YouTube-Tracker-Keyword-API/YouTubeKeywordTrackerClientApp/client-app/src/app/providers/authorization.interceptor.ts
+++ YouTube-Tracker-Keyword-API/YouTubeKeywordTrackerClientApp/client-app/src/app/providers/authorization.interceptor.ts
@@ -1,14 +1,19 @@
 import { Injectable } from "@angular/core";
-import { Store } from "@ngrx/store/src";
 import { HttpRequest } from "@angular/common/http";
 import { HttpInterceptor } from "@angular/common/http";
 import { HttpHandler } from "@angular/common/http";
 import { Observable } from "rxjs";
 import { HttpEvent } from "@angular/common/http";
+import { Store } from "@ngrx/store";
+import { logout } from "../state/auth/auth.actions";
+import { ToastService } from "../services/toast.service";
+import { LogoutActionMessages } from "../models/toast/toast-messages";
+import { tap } from "rxjs";
+import { IState } from "../state";
 
 @Injectable()
 export class AuthorizationInterceptor implements HttpInterceptor {
-    constructor() {}
+    constructor(private store: Store<IState>, private toastService: ToastService) {}
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         let currentTokenString = localStorage.getItem('currentToken');
@@ -16,7 +21,18 @@ export class AuthorizationInterceptor implements HttpInterceptor {
         if (currentToken && currentToken.length > 0 && currentToken !== null) {
             const authReq = request.clone({ setHeaders: { Authorization: 'Bearer ' + currentToken } });
 
-            return next.handle(authReq);
+            return next.handle(authReq).pipe(
+                tap(
+                    () => {},
+                    (error: any) => {
+                        if (error.status === 401) {
+                            this.store.dispatch(logout());
+                            this.toastService.error(LogoutActionMessages.NotAuthorized);
+                            return;
+                        }
+                    }
+                )
+            );
         } else {
             return next.handle(request);
         }
