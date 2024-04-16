b386bd8692397b579c65896cb28ccfae5753a52b
sekvi <filmatkozlik@gmail.com>
2024-04-16 20:25:20 +0200

    Temporary fix for report details

--- YouTube-Tracker-Keyword-API/YouTubeKeywordTrackerClientApp/client-app/src/app/pages/reports/report-details/report-details.component.ts
+++ YouTube-Tracker-Keyword-API/YouTubeKeywordTrackerClientApp/client-app/src/app/pages/reports/report-details/report-details.component.ts
@@ -1,13 +1,19 @@
 import { Component, ViewChild } from '@angular/core';
 import { Subscription, BehaviorSubject } from 'rxjs';
-import { IReportDetail, IReportDetails } from '../../../models/report/report-details';
+import {
+  IReportDetail,
+  IReportDetails,
+} from '../../../models/report/report-details';
 import { DataService } from '../../../services/data.service';
 import { Store } from '@ngrx/store';
 import { IState } from '../../../state';
 import { ActivatedRoute } from '@angular/router';
 import { ReportsEndpoints } from '../../../services/api-endpoints/endpoints';
 import { HEADER_DEFINITIONS } from '../../../common/header/header-definitions';
-import { GetDataButtonLabels, MailButtonLabels } from '../../../common/buttons/add-data-button/add.data.buttons';
+import {
+  GetDataButtonLabels,
+  MailButtonLabels,
+} from '../../../common/buttons/add-data-button/add.data.buttons';
 import { PdfPreviewComponent } from '../../../common/pdf-preview/pdf-preview.component';
 import { IReportFile } from '../../../models/report/report-file';
 import { ToastService } from '../../../services/toast.service';
@@ -18,35 +24,43 @@ import { MailDialogComponent } from '../../../common/mail/mail-dialog/mail-dialo
 @Component({
   selector: 'app-report-details',
   templateUrl: './report-details.component.html',
-  styleUrl: './report-details.component.scss'
+  styleUrl: './report-details.component.scss',
 })
 export class ReportDetailsComponent {
   private isFetchingFileSubject = new BehaviorSubject<boolean>(false);
   isFetchingFile$ = this.isFetchingFileSubject.asObservable();
-  
+
   reportData!: IReportDetails;
-  private subscription!: Subscription;
+  private subscription: Subscription = new Subscription();
 
   headerDefinition = HEADER_DEFINITIONS.reportDetails;
   raportFileButtonLabel = GetDataButtonLabels.GetReportFile;
   reportReadoutsColumns = REPORT_READOUTS_COLUMNS_DEFINITION;
   sentReportsViaMailButtonLabel = MailButtonLabels.SentReportViaMail;
 
-  @ViewChild(PdfPreviewComponent, { static: true }) fileDialog!: PdfPreviewComponent;
-  @ViewChild(MailDialogComponent, { static: true }) mailDialog!: MailDialogComponent;
+  @ViewChild(PdfPreviewComponent, { static: true })
+  fileDialog!: PdfPreviewComponent;
+  @ViewChild(MailDialogComponent, { static: true })
+  mailDialog!: MailDialogComponent;
 
-  constructor(private dataService: DataService, private store: Store<IState>, private route: ActivatedRoute, private toastService: ToastService) {}
+  constructor(
+    private dataService: DataService,
+    private route: ActivatedRoute,
+    private toastService: ToastService
+  ) {}
 
   ngOnInit() {
-    this.subscription = this.route.paramMap.subscribe(params => {
+    let sub = this.route.paramMap.subscribe((params) => {
       const reportId = params.get('id');
       if (reportId) {
-        this.dataService.fetch<IReportDetails>(`${ReportsEndpoints.Report}/${reportId}`)
-          .subscribe(response => {
+        this.dataService
+          .fetch<IReportDetails>(`${ReportsEndpoints.Report}/${reportId}`)
+          .subscribe((response) => {
             this.reportData = response;
           });
       }
     });
+    this.subscription.add(sub);
   }
 
   ngOnDestroy() {
@@ -55,17 +69,21 @@ export class ReportDetailsComponent {
 
   onDownloadReportClick(event: any): void {
     this.isFetchingFileSubject.next(true);
-    this.dataService.fetch<IReportFile>(`${ReportsEndpoints.ReportFile}/${this.reportData.fileId}`)
-    .subscribe({
-      next: (response) => {
-        this.fileDialog.openDialog(response.fileContent);
-        this.isFetchingFileSubject.next(false);
-      },
-      error: () => {
-        this.toastService.error(FileMessages.FetchError);
-        this.isFetchingFileSubject.next(false);
-      }
-    })
+    let downloadSub = this.dataService
+      .fetch<IReportFile>(
+        `${ReportsEndpoints.ReportFile}/${this.reportData.fileId}`
+      )
+      .subscribe({
+        next: (response) => {
+          this.fileDialog.openDialog(response.fileContent);
+          this.isFetchingFileSubject.next(false);
+        },
+        error: () => {
+          this.toastService.error(FileMessages.FetchError);
+          this.isFetchingFileSubject.next(false);
+        },
+      });
+    this.subscription.add(downloadSub);
   }
 
   onVideoOpenClick(video: IReportDetail): void {
